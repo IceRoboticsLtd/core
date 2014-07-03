b5073bc813454407a86d52031dc60f9167118bef
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-03 09:00:08 +0100

    Initial commit

---
+++ pre-public/js/app/modelEvent/Base.js
@@ -0,0 +1,9 @@
+define(function () {
+    function modelEventBase(id) {
+        this.id = id;
+    }
+    modelEventBase.prototype = {
+		// to do
+    };
+    return modelEventBase;
+});
---
+++ pre-public/js/app/modelEvent/modelEvent.js
@@ -0,0 +1,28 @@
+/*
+ * ModelEvent
+ * An event is where something happening is captured.
+ */
+
+var ModelEvent123 = 1 << 0;
+var ModelEvent456 = 1 << 1;
+
+define(['./Base'], function (Base) {
+    var _ModelEvent = new Base('');
+
+    // following this example, slightly
+    // http://sandbox.thewikies.com/javascript-mvc-hello-world/index.2.html
+
+    _ModelEvent.raiseEvent = function ( flag ) {
+        // Check if ModelEvent123 was passed.
+        if (flag & ModelEvent123) {
+            // Run the ModelController's show function.
+            _ModelController.loadModel(123);
+        }
+        // Check if ModelEvent456 was passed.
+        if (flag & ModelEvent456) {
+            // Run the ModelController's show function.
+            _ModelController.loadModel(456);
+        }
+    };
+    return _ModelEvent;
+});
