9e2ec000e24613ffe01349886ce8eade74920af8
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-03 07:53:45 +0100

    Updated

--- pre-public/js/app/main.js
+++ pre-public/js/app/main.js
@@ -5,17 +5,22 @@ define(function (require) {
         controller = require('./controller/controller'),
         model = require('./model/model'),
 		modelController = require('./modelController/modelController'),
-		modelService = require('./modelService/modelService'),
-		serviceBus = require('./serviceBus/serviceBus'),
-		view = require('./view/view'),
-		viewController = require('./viewController/viewController'),
-		viewService = require('./viewService/viewService'),		
+//      modelEvent = require('./modelEvent/modelEvent'),
+//		modelService = require('./modelService/modelService'),
+//		serviceBus = require('./serviceBus/serviceBus'),
+//		view = require('./view/view'),
+//		viewController = require('./viewController/viewController'),
+//      viewEvent = require('./viewEvent/viewEvent'),
+//		viewService = require('./viewService/viewService'),	
+       	
         backbone = require('backbone'),
-        underscore = require('underscore'),
+        underscore = require('underscore');
+       
         //lodash = require('lodash'), //WAS require('./../../../bower_components/lodash/dist/lodash'),
-		postal = require('postal');
+//		postal = require('postal');
         //postaldiags = require('postal.diagnostics'), //WAS require('./../../../bower_components/postal.diagnostics/lib/postal.diagnostics.min'),
         //conduit = require('conduitjs'); //WAS require('./../../../bower_components/conduitjs/lib/conduit.min');
+
 
     //A fabricated API to show interaction of
     //common and specific pieces.
@@ -24,7 +29,6 @@ define(function (require) {
     controller.setModel(model);
     $(function () {
         controller.render(lib.getBody());
-
         //Display backbone and underscore versions
         $('body')
             .append('<div>backbone version: ' + backbone.VERSION + '</div>')
