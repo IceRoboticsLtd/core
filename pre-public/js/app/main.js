57b29c812fbfe87ab142b3a201240d009bedc304
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-06 23:59:41 +0100

    Updated

--- pre-public/js/app/main.js
+++ pre-public/js/app/main.js
@@ -17,14 +17,20 @@ define(function (require) {
         viewEvent = require('./viewEvent/viewEvent'),
 	    viewService = require('./viewService/viewService'),
         backbone = require('backbone'),
-        underscore = require('underscore/underscore'),
-//        lodash = require('../lodash/dist/lodash'),
-//        jquery = require('jquery'),
-//        conduit = require('conduit'),
-//        conduitjs = require('conduitjs');        
-        postal = require('postal/postal');
-//        postaldiags = require('postal.diagnostics');
+        underscore = require('underscore'),
+        lodash = require('lodash'),
+        jquery = require('jquery'),
+        bootstrap = require('bootstrap'); // bootstrap extends jquery
+        expect = require('expect'),
+        mocha = require('mocha'),
+        conduitjs = require('conduit'); // depends on bootstrap, expect, and mocha
+//        postal = require('postal')(lodash), // depends on lodash
+//        postaldiags = require('postaldiags')(postal); // depends on postal and lodash
 
+    //    postal: "../../../lib/postal",
+    //    postaldiags: "../../../bower/postal.diagnostics/lib/postal.diagnostics",
+     
+    //    postal = require('postal');
 
     // Set config
     console.log('config:');
@@ -34,21 +40,31 @@ define(function (require) {
     console.log('backbone:');
     console.log(backbone);
     // Lodash check
- //   console.log('lodash:');
- //   console.log(lodash);
+    console.log('lodash:');
+    console.log(lodash);
     // JQuery check
- //   console.log('jquery:');
- //   console.log(jquery); 
+    console.log('jquery:');
+    console.log(jquery); 
+    jquery.VERSION = jquery.fn.jquery;
+    // Bootstrap check
+    console.log('bootstrap:');
+    console.log(bootstrap);
+    // Expect check
+    console.log('expect:');
+    console.log(expect);
+    // Mocha check
+    console.log('mocha:');
+    console.log(mocha);
     // ConduitJS check
-//    console.log('conduitjs:');
-//    console.log(conduitjs);
+    console.log('conduitjs:');
+    console.log(conduitjs);
+
     // Postal check
-    console.log('postal');
-    console.log(postal);
+//    console.log('postal');
+//    console.log(postal);
     // PostalDiags check
 //    console.log('postaldiags:');
 //    console.log(postaldiags);
-
 
 //    serviceBus.serviceBus = postal; // Set postal as the serviceBus property of serviceBus
 //    controller.setServiceBus(serviceBus);
@@ -59,17 +75,26 @@ define(function (require) {
 
     //A fabricated API to show interaction of
     //common and specific pieces.
+
+    // DOM ready
     $(function () {
         controller.render(lib.getBody());
         //Display backbone and underscore versions
         $('body')
             .append('<div>backbone version: ' + backbone.VERSION + '</div>')
             .append('<div>underscore version: ' + underscore.VERSION + '</div>')
-            .append('<div>postal version: ' + postal.VERSION + '</div>')                               
+            .append('<div>lodash version: ' + lodash.VERSION + '</div>')
+            .append('<div>jquery version: ' + jquery.VERSION + '</div>')
+            .append('<div>expect version: ' + expect.VERSION + '</div>')
+            .append('<div>mocha version: ' + mocha.VERSION + '</div>')
+            .append('<div>conduitjs version: ' + conduitjs.VERSION + '</div>')
+        //    .append('<div>postal version: ' + postal.VERSION + '</div>')              
+        //    .append('<div>postaldiags version: ' + postaldiags.VERSION + '</div>')                                       
 			.append('<div>Example 1 - The World\'s Simplest Subscription<div class="results" id="example1"></div></div>');
     });
 
 /*
+    // DOM ready
     $(function() {
         require(["calculator"], function(calculator) {
             console.log('calculator is required');
--- pre-public/js/common.js
+++ pre-public/js/common.js
@@ -8,15 +8,39 @@
 requirejs.config({
     baseUrl: 'js/lib',
     paths: {
-        app: '../app'
+        app: '../app',
+        lodash: 'postal/bower/lodash/dist/lodash',
+        expect: 'postal/bower/expect/expect',
+        mocha: 'postal/bower/mocha/mocha',
+        jquery: 'postal/bower/jquery/jquery',  
+        bootstrap: 'postal/bower/bootstrap/dist/js/bootstrap',
+        conduitjs: 'postal/bower/conduitjs/lib/conduit'
     },
+    // A shim automatically adds a wrapper around a javascript library 
+    // that makes it AMD-compatible, requirejs-friendly.
     shim: {
         backbone: {
-            deps: ['jquery', 'underscore'],
+            deps: ['jquery','underscore'],
             exports: 'Backbone'
         },
         underscore: {
             exports: '_'
-        }       
+        },
+        lodash: {
+            exports: 'lodash'
+        },
+        expect: {
+            exports: 'expect'
+        },
+        mocha: {
+            exports: 'mocha'
+        },        
+        bootstrap: {
+            deps: ['jquery'] // bootstrap has nothing to export
+        },
+        conduitjs: {
+            deps: ['bootstrap','expect','mocha'],
+            exports: 'conduitjs'
+        }    
     }
 });
