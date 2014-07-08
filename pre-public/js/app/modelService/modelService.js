6770c08ea84ef2ee990adfa7cc23c6d08e73bd8c
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-08 02:24:50 +0100

    Updated

--- pre-public/js/app/modelService/modelService.js
+++ pre-public/js/app/modelService/modelService.js
@@ -3,7 +3,7 @@
  */
 define(['./Base'], function (Base) {
     console.log('CORE: modelService called');	
-    var _ModelService = new Base('');
+    var _ModelService = new Base('ModelService');
 
     // following this example, slightly
     // http://sandbox.thewikies.com/javascript-mvc-hello-world/index.2.html
@@ -20,7 +20,7 @@ define(['./Base'], function (Base) {
 //    channel.publish({ name: "Dr. Who" });
 
 	// A modelService constructor might have a function that creates new modelService instances
-	_ModelService.find = function ( id ) {
+	_ModelService.find = function (id) {
 		console.log('CORE: modelService find(id) called');
 		// Data used to create a new modelService may come from anywhere
 		// but in this case data comes from this inline object.
