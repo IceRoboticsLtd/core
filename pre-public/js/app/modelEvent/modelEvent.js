ac3560cd3e5114589036d17cfdb6c96f5e20758a
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-12 17:31:13 +0100

    Updated

--- pre-public/js/app/modelEvent/modelEvent.js
+++ pre-public/js/app/modelEvent/modelEvent.js
@@ -8,7 +8,13 @@ var ModelEventSubscribe = 1 << 2;
 
 define(['./Base'], function (Base) {
     console.log('CORE: modelEvent called');
-    var _ModelEvent = new Base('ModelEvent');
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+    });    
+    var _ModelEvent = new Base(uuid);
 
     // following this example, slightly
     // http://sandbox.thewikies.com/javascript-mvc-hello-world/index.2.html
