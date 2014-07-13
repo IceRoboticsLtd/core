7cdf5967c447e10bfe57b0f9daf93182bd9970a3
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-13 22:51:21 +0100

    Updated

--- pre-public/js/app/modelService/Base.js
+++ pre-public/js/app/modelService/Base.js
@@ -15,14 +15,23 @@ define(function () {
     		// created from the serviceBus's "yourProperty".
 			this.myProperty = this.serviceBus.yourProperty;
 		},
-		setSubscriptions: function (subscriptions){
+		setSubscriptions: function (subscriptions) {
 			console.log('CORE: modelServiceBase setSubscriptions(subscriptions) called');
 			this.subscriptions = subscriptions;
 		},
 		subscribe: function () {
 			console.log('CORE: modelServiceBase subscribe() called');	
 			// Subscribe to the serviceBus with channels and topics from subscriptions
-			// TO DO
+			this.subscriptions.forEach( function (subscription) {
+				for (key in subscription) {
+					if(key == 'channel') {
+						var channel = subscription[key];
+						console.log('CORE: modelServiceBase channel:');
+						console.log(channel);
+						// TO DO
+					}
+				}
+			});
 		}
     };   
     return modelServiceBase;
