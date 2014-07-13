cd3ea77425f3bbc6547705158ca665928915faa7
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-13 05:57:28 +0100

    Updated

--- pre-public/js/app/serviceBus/Base.js
+++ pre-public/js/app/serviceBus/Base.js
@@ -18,13 +18,74 @@ define(function () {
 		};// eof bindingsResolver
 
     };
+    console.log('******** I AM HERE: line AA **********');
+    /*
+     * Channel
+     */
+    var ChannelDefinition = function (channelName) {
+   		console.log('CORE: ServiceBusBase ChannelDefinition(channelName) called'); 	
+    //    this.channel = channelName || this.configuration.DEFAULT_CHANNEL;
+    //    this.initialize();
+    };
+    console.log('******** I AM HERE: line BB **********');    
+    ChannelDefinition.prototype.initialize = function () {
+    	console.log('CORE: ServiceBusBase ChannelDefinition.prototype.initialize() called');
+        // to do
+    };
+    console.log('******** I AM HERE: line CC **********');    
+    ChannelDefinition.prototype.subscribe = function () {
+    	console.log('CORE: ServiceBusBase ChannelDefinition.prototype.subscribe() called');
+    	// to do
+    };
+    console.log('******** I AM HERE: line DD **********');    
+    ChannelDefinition.prototype.publish = function () {
+    	console.log('CORE: ServiceBusBase ChannelDefinition.prototype.publish() called');
+    	// to do
+    };
+    console.log('******** I AM HERE: line EE **********');
+    /*
+     * Subscription
+     */
+    var SubscriptionDefinition = function (channel, topic, callback) {
+    	console.log('CORE: ServiceBusBase SubscriptionDefinition(channel, topic, callback) called');
+        if (arguments.length !== 3) {
+            throw new Error("You must provide a channel, topic and callback when creating a SubscriptionDefinition instance.");
+        }
+        if (topic.length === 0) {
+            throw new Error("Topics cannot be empty");
+        }
+        this.channel = channel;
+        this.topic = topic;
+        this.subscribe(callback);
+    };
+    console.log('******** I AM HERE: line FF **********');    
+    SubscriptionDefinition.prototype = {
+
+    	// to do
+
+		subscribe: function (callback) {
+            // to do
+        }
+
+    	// to do
+    };
+    console.log('******** I AM HERE: line GG **********');
+
+
+
+    console.log('******** I AM HERE: line HH **********');
+
     serviceBusBase.prototype = {
 		configuration: {
             resolver: this.bindingsResolver,
             DEFAULT_CHANNEL: "/",
             SYSTEM_CHANNEL: "serviceBus"
-        }
-
+        },
+        subscriptions: {},
+        wireTaps: [],
+        ChannelDefinition: ChannelDefinition,
+        SubscriptionDefinition: SubscriptionDefinition
+        // to do
 
     };
     return serviceBusBase;
--- pre-public/js/app/serviceBus/serviceBus.js
+++ pre-public/js/app/serviceBus/serviceBus.js
@@ -10,69 +10,80 @@ define(['./Base'], function (Base) {
     	return (c=='x' ? r : (r&0x7|0x8)).toString(16);
     });
     var _ServiceBus = new Base(uuid);
+
+
+    console.log('******** I AM HERE: line AAA **********');
+
+
     /*
      * Conduit
 	 */
 	function Conduit(options) { 
+		console.log('CORE: ServiceBus Conduit(options) called');
+		if (typeof options.target !== "function") {
+            throw new Error("You can only make functions into Conduits.");
+        }
+        var _steps = {
+            pre: options.pre || [],
+            post: options.post || [],
+            all: []
+        };
+        var _defaultContext = options.context;
+        var _target = options.target;
+        var _targetStep = {
+            isTarget: true,
+            fn: options.sync ?
+            function () {
+                var args = Array.prototype.slice.call(arguments, 0);
+                var result = _target.apply(_defaultContext, args);
+                return result;
+            } : function (next) {
+                var args = Array.prototype.slice.call(arguments, 1);
+                args.splice(1, 1, _target.apply(_defaultContext, args));
+                next.apply(this, args);
+            }
+        };
+        var _genPipeline = function () {
+            _steps.all = _steps.pre.concat([_targetStep].concat(_steps.post));
+        };
+        _genPipeline();
+		var conduit = function () {
+			// to do
+		};	
+		return conduit;
+    };
+	Conduit.Sync = function (options) {
+    	console.log('CORE: ServiceBus Conduit.Sync(options) called');
+        options.sync = true;
+        return Conduit.call(this, options)
+    };
+	Conduit.Async = function (options) {
+    	console.log('CORE: ServiceBus Conduit.Async(options) called');
+        return Conduit.call(this, options);
+    };
+
+    console.log('******** I AM HERE: line BBB **********');	
+    function subscribe(options) {
+    	console.log('CORE: ServiceBusBase subscribe(options) called');
+        var subDef = new SubscriptionDefinition(options.channel || _ServiceBus.configuration.DEFAULT_CHANNEL, options.topic, options.callback);    	
 		// to do
-	    return {
-	        Sync: function (options) {
-	            options.sync = true;
-	            return Conduit.call(this, options)
-	        },
-	        Async: function (options) {
-	            return Conduit.call(this, options);
-	        }
-	    }
-	};
-    /*
-     * Channel
-     */
-    var ChannelDefinition = function (channelName) {
-        this.channel = channelName || _ServiceBus.configuration.DEFAULT_CHANNEL;
-        this.initialize();
+
+        return subDef;
     };
-    ChannelDefinition.prototype.initialize = function () {
-    	console.log('CORE: ServiceBus ChannelDefinition.prototype.initialize() called');
-        // to do
-    };
-    ChannelDefinition.prototype.subscribe = function () {
-    	console.log('CORE: ServiceBus ChannelDefinition.prototype.subscribe() called');
-    	// to do
-    };
-    ChannelDefinition.prototype.publish = function () {
-    	console.log('CORE: ServiceBus ChannelDefinition.prototype.publish() called');
-    	// to do
-    };
-    /*
-     * Subscription
-     */
-    var SubscriptionDefinition = function (channel, topic, callback) {
-        if (arguments.length !== 3) {
-            throw new Error("You must provide a channel, topic and callback when creating a SubscriptionDefinition instance.");
-        }
-        if (topic.length === 0) {
-            throw new Error("Topics cannot be empty");
-        }
-        this.channel = channel;
-        this.topic = topic;
-        this.subscribe(callback);
-    };
-    SubscriptionDefinition.prototype = {	
-    	// to do
-    };
+    console.log('******** I AM HERE: line CCC **********');
     /*
      * ServiceBus Functions
 	 */
-/*	 
+
+	console.log('Conduit');
+	console.log(Conduit);
+
 	_ServiceBus.subscribe = new Conduit.Sync({
         target: subscribe,
         context: _ServiceBus
     });
-*/
 
 
-
-
+    console.log('******** I AM HERE: line ZZZ **********');
     return _ServiceBus;
 });
