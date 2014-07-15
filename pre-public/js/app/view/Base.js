9d28561a5c2e5cbb57fc435a7ebc9e356c44cc10
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-15 21:46:55 +0100

    Updated

--- pre-public/js/app/view/Base.js
+++ pre-public/js/app/view/Base.js
@@ -2,16 +2,32 @@
  * ViewBase
  */
 define(function () {
-    console.log('CORE: ViewBase called');	
+    console.log('CORE: ViewBase called');   
     function viewBase(id) {
         this.id = id;
-        this.keyValuePairs = {};
+        this.keyValuePairs = {};     
     };
     viewBase.prototype = {
         getValue: function (key) {
-            console.log('CORE: ViewBase getValue(key) called'); 
-            // To Do: search for key in keyValuePairs and retrieve its value           
-            // return value;
+            console.log('CORE: ViewBase getValue(key) called');
+            console.log('key = ' + key);
+            var searchKey = key;
+            var keyFound = false;
+            var value;
+            for(key in this.keyValuePairs) {
+                if(key == searchKey){
+                    keyFound = true;
+                    value = this.keyValuePairs[key];
+                }
+            }
+            if(keyFound) {
+                console.log('key found');
+            }
+            else {
+                console.log('key not found');
+            }
+            console.log('value = ' + value);
+            return value;
         },
         setValue: function (key, value) {
             console.log('CORE: ViewBase setValue(key, value) called'); 
@@ -25,30 +41,56 @@ define(function () {
         setKeyValuePairs: function (keyValuePairs) {
             console.log('CORE: ViewBase setKeyValuePairs(keyValuePairs) called');         
             this.keyValuePairs = keyValuePairs;
+        },      
+        setViewService: function (viewService) {
+            console.log('CORE: ViewBase setViewService(viewService) called');
+            // The view instance has a property called "viewService"
+            // created from the viewService.                
+            this.viewService = viewService;
         },
-		setViewService: function (viewService) {
-			console.log('CORE: ViewBase setViewService(viewService) called');
-    		// The view instance has a property called "viewService"
-    		// created from the viewService.				
-			this.viewService = viewService;
-		},
-		// A view might have a function that returns the rendered output.
-		output: function() {
-			// Data used to create a template may come from anywhere
-			// but in this case template comes from the inline string.
-			var ourData = '<h1><%= myProperty %></h1>';
-			// Store the instance for reference in the replace function below.
-			var instance = this;
-			// Return the template using the values from the viewService.
-			return ourData.replace(/<%=\s+(.*?)\s+%>/g, function (m,m1) {
-				return instance.viewService[m1];
-			});
-		},
-		// A view might have a function that renders the output.
-		render: function () {
-			// This view renders to the element with the id of "output".
-			document.getElementById('output').innerHTML = this.output();
-		}
+        // A view might have a function that returns the rendered output.
+        getView: function(key) {
+            console.log('CORE: ViewBase getView() called');
+
+    //      // Data used to create a template may come from anywhere
+    //      // but in this case template comes from the inline string.
+    //      var ourData = '<h1><%= myProperty %></h1>';
+    //      // Store the instance for reference in the replace function below.
+    //      var instance = this;
+    //      // Return the template using the values from the viewService.
+    //      return ourData.replace(/<%=\s+(.*?)\s+%>/g, function (m,m1) {
+    //          return instance.viewService[m1];
+    //      });
+            try {
+                this.view = this.getValue(key);
+                console.log('CORE: ViewBase this.view:');
+                console.log(this.view);
+                return this.view;
+            }
+            catch(e) {
+                console.log('CORE: ViewBase getView(key) error:');
+                console.log(e);
+                return;
+            }
+        },
+        // A view might have a function that renders the view.
+        renderView: function (elementId) {
+            console.log('CORE: ViewBase renderView(elementId) called');
+            console.log('elementId = ' + elementId);
+            // This view renders to the element with the id provided.
+            try {
+                document.getElementById(elementId).innerHTML = this.getView('htmlSource');
+                // Add the view's script element
+                var scriptTag = document.createElement('script');
+                scriptTag.type = "text/javascript";
+                scriptTag.src = this.getValue('scriptSource');
+                document.getElementsByTagName('head')[0].appendChild(scriptTag);
+            }
+            catch(e) {
+                console.log('CORE: ViewBase renderView(elementId) error:');
+                console.log(e);
+            }
+        }
     };
     return viewBase;
 });
