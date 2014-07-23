8116bc03c2d3d138e6dfbbc7095136ce7dd43188
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-23 09:21:59 +0100

    Updated

--- pre-public/js/calculator/models/Base.js
+++ pre-public/js/calculator/models/Base.js
@@ -3,7 +3,8 @@
  */
 define(function () {
     console.log('CORE: Base called');	
-    function Base(configs) {
+    function Base(id, configs) {
+        this.id = id;
         this.configs = configs;
     };
     configBase.prototype = {
