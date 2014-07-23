7f69b3be1b35f605a45f10c5a110c9c35f0e08d0
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-23 23:18:04 +0100

    Updated

--- pre-public/js/app/model/Base.js
+++ pre-public/js/app/model/Base.js
@@ -8,18 +8,26 @@ define(function () {
         this.keyValuePairs = {};
     };
     modelBase.prototype = {
-        getTitle: function () {
-            console.log('CORE: modelBase getTitle() called');            
-            return this.title;
-        },
-        setTitle: function (title) {
-            console.log('CORE: modelBase setTitle(title) called');            
-            this.title = title;
-        },
         getValue: function (key) {
-            console.log('CORE: modelBase getValue(key) called'); 
-            // To Do: search for key in keyValuePairs and retrieve its value           
-            // return value;
+            console.log('CORE: modelBase getValue(key) called');
+            console.log('key = ' + key);
+            var searchKey = key;
+            var keyFound = false;
+            var value;
+            for(key in this.keyValuePairs) {
+                if(key == searchKey){
+                    keyFound = true;
+                    value = this.keyValuePairs[key];
+                }
+            }
+            if(keyFound) {
+                console.log('key found');
+            }
+            else {
+                console.log('key not found');
+            }
+            console.log('value = ' + value);
+            return value;
         },
         setValue: function (key, value) {
             console.log('CORE: modelBase setValue(key, value) called'); 
@@ -37,6 +45,33 @@ define(function () {
         setModelService: function (modelService) {
             console.log('CORE: modelBase setModelService(modelService) called');          
             this.modelService = modelService;
+        },
+        getModel: function(key) {
+            console.log('CORE: modelBase getModel() called');
+            try {
+                this.model = this.getModel(key);
+                console.log('CORE: modelBase this.model:');
+                console.log(this.model);
+                return this.model;
+            }
+            catch(e) {
+                console.log('CORE: modelBase getModel(key) error:');
+                console.log(e);
+                return;
+            }
+        },
+        renderModel: function () {
+            console.log('CORE: modelBase renderModel() called');
+            try {
+                var scriptTag = document.createElement('script');
+                scriptTag.type = "text/javascript";
+                scriptTag.src = this.getValue('scriptSource');
+                document.getElementsByTagName('head')[0].appendChild(scriptTag);
+            }
+            catch(e) {
+                console.log('CORE: modelBase renderModel() error:');
+                console.log(e);
+            }
         }
     };
     return modelBase;
