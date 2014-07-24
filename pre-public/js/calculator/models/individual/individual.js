b24201c7fcfcc0fa115f15aee3abf306106d8c6f
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-24 21:14:30 +0100

    Updated

--- pre-public/js/calculator/models/individual/individual.js
+++ pre-public/js/calculator/models/individual/individual.js
@@ -1,55 +1,51 @@
 /*
  * Individual
  */
-define(['./Base'], function (Base) {
-    console.log('CORE: config called');
-    var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    	var r = (d + Math.random()*16)%16 | 0;
-    	d = Math.floor(d/16);
-    	return (c=='x' ? r : (r&0x7|0x8)).toString(16);
-    });    
-    var configs = {
-    	// to do
-	};
-    var _Individual = new Base(uuid, configs);
+/////////////////////////// ON DOCUMENT READY /////////////////////////////
+$(document).ready(function() {
+    console.log('CORE: calculator model Individual: document ready');
+    require(['../calculator/models/individual/Base'], function (Base) {
+        console.log('CORE: individual called');
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+        });    
+        var configs = {
+            // to do
+        };
+        var Individual = new Base(uuid, configs);
+        // Initial Setup
+        // -------------
 
-    // Initial Setup
-    // -------------
+        // Save the previous value of the `Individual` variable, so that it can be
+        // restored later on, if `noConflict` is used.
+        var previousIndividual = global.Individual;
 
-    // Save the previous value of the `_Individual` variable, so that it can be
-    // restored later on, if `noConflict` is used.
-    var previousIndividual = global._Individual;
+        // Create local references to array methods we'll want to use later.
+        var array = [];
+        var push = array.push;
+        var slice = array.slice;
+        var splice = array.splice;
 
-    // Create local references to array methods we'll want to use later.
-    var array = [];
-    var push = array.push;
-    var slice = array.slice;
-    var splice = array.splice;
+        // Current version of the library. Keep in sync with `package.json`.
+        Individual.VERSION = '1.1.0';
 
-    // Current version of the library. Keep in sync with `package.json`.
-    _Individual.VERSION = '1.1.0';
+        // For Individual's purposes, jQuery, Zepto, or Ender owns the `$` variable.
+        Individual.$ = $;
 
-    // For Individual's purposes, jQuery, Zepto, or Ender owns the `$` variable.
-    _Individual.$ = $;
-
-    // Runs Individual.js in *noConflict* mode, returning the `_Individual` variable
-    // to its previous owner. Returns a reference to this Individual object.
-    _Individual.noConflict = function() {
-        global._Individual = previousIndividual;
-        return this;
-    };
-    
-
-
-
+        // Runs Individual.js in *noConflict* mode, returning the `_Individual` variable
+        // to its previous owner. Returns a reference to this Individual object.
+        Individual.noConflict = function() {
+            global.Individual = previousIndividual;
+            return this;
+        };
 
 
+        // more
 
 
-
-
-
-
-    return _Individual;
-}); 
+        return Individual;         
+    });
+});
