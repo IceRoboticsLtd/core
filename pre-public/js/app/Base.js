048d78f63afe1e0a0cd1f2fb9d81e96d97d16b6c
Willem van Heemstra <wvanheemstra@icloud.com>
2015-01-11 18:09:24 +0000

    Updated

--- pre-public/js/app/Base.js
+++ pre-public/js/app/Base.js
@@ -208,6 +208,10 @@ define(function () {
                     console.log('CORE: mainBase: pageRouter required');
                     console.log(pageRouter);
                     me.pageRouter = pageRouter; 
+                    // Adopted from Backbone
+                    // Set up inheritance for the router and history.
+                    me.pageRouter.extend = mainBase.extend; 
+                    me.pageRouter.history.extend = mainBase.extend;
                     callback(me, err, me.pageRouter);
                 });
             }
@@ -276,6 +280,10 @@ define(function () {
                     console.log('CORE: mainBase: model required');
                     console.log(model);
                     me.model = model; 
+                    // Adopted from Backbone
+                    // Set up inheritance for the model and collection.
+                    me.model.extend = mainBase.extend; 
+                    me.model.model.Collection.extend = mainBase.extend;
                     callback(me, err, me.model);
             	});
             }
@@ -344,6 +352,9 @@ define(function () {
                     console.log('CORE: mainBase: view required');
                     console.log(view);
                     me.view = view; 
+                    // Adopted from Backbone
+                    // Set up inheritance for the view.
+                    me.view.extend = mainBase.extend; 
                     callback(me, err, me.view);
             	});
             }
@@ -387,6 +398,55 @@ define(function () {
         		callback(me, err, me.viewEvent);
         	}
     	},
+        // Adopted from Backbone
+        // Helpers
+        // -------
+        // Helper function to correctly set up the prototype chain, for subclasses.
+        // Similar to `goog.inherits`, but uses a hash of prototype properties and
+        // class properties to be extended.        
+        extend: function(protoProps, staticProps) {
+            console.log('CORE: mainBase extend(protoProps, staticProps) called');
+            var parent = this;
+            var child;
+            // The constructor function for the new subclass is either defined by you
+            // (the "constructor" property in your `extend` definition), or defaulted
+            // by us to simply call the parent's constructor.
+            if (protoProps && _.has(protoProps, 'constructor')) {
+                child = protoProps.constructor;
+            } else {
+                child = function(){ return parent.apply(this, arguments); };
+            }
+            // Add static properties to the constructor function, if supplied.
+            _.extend(child, parent, staticProps);
+            // Set the prototype chain to inherit from `parent`, without calling
+            // `parent`'s constructor function.
+            var Surrogate = function(){ this.constructor = child; };
+            Surrogate.prototype = parent.prototype;
+            child.prototype = new Surrogate;
+            // Add prototype properties (instance properties) to the subclass,
+            // if supplied.
+            if (protoProps) _.extend(child.prototype, protoProps);
+            // Set a convenience property in case the parent's prototype is needed
+            // later.
+            child.__super__ = parent.prototype;
+            return child;
+        },
+        // Adopted from Backbone
+        // Throw an error when a URL is needed, and none is supplied.
+        urlError: function() {
+            console.log('CORE: mainBase urlError() called');            
+            throw new Error('A "url" property or function must be specified');
+        },
+        // Adopted from Backbone
+        // Wrap an optional error callback with a fallback error event.
+        wrapError: function(model, options) {
+            console.log('CORE: mainBase wrapError(model, options) called');            
+            var error = options.error;
+            options.error = function(resp) {
+                if (error) error(model, resp, options);
+                model.trigger('error', model, resp, options);
+            };
+        },      
         ready: function() {
         	console.log('CORE: mainBase ready() called');
         	var me = this; // contains the current context
