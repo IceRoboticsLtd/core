51ab6ebd75fbedc7693a5df994816a5547741fb9
Willem van Heemstra <wvanheemstra@icloud.com>
2015-02-08 18:38:12 +0000

    Updated

--- pre-public/js/app/page/page.js
+++ pre-public/js/app/page/page.js
@@ -1,23 +1,20 @@
 /*
  * Page
  */
-var QuizEngine = (function(){
-	console.log('CORE: Page called');
-    var Application = Marionette.Application.extend({});
-
-    var application = new Application();
-
-    application.addRegions({
-        header: '[data-region=header]', // Not used right now
-        body: '[data-region=body]',
-        footer: '[data-region=footer]' // Not used right now
-    });
-
-    // application.on('initialize:after', function() { // Marionette 1 version
-	application.on('start', function() { // Marionette 2 version
-        console.log('CORE: Page start event caught');
-        Backbone.history.start();
-    });
-
-    return application;
-})();
+define(['./Base'], function (Base) {
+    console.log('CORE: page called');
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+    });  
+    // ORIGINAL var _Page = new Base(uuid);    
+    var attributes = attributes || {};
+    var options = options || {};       
+    var _Page = new Base(uuid, attributes, options); // extended for arguments & options to adopt Backbone       
+    // Make Application available as a direct property of CORE
+//    CORE.Application = _Page.initApplication();    // TEMP COMMENTED OUT, FOR IT GIVES ERRORS
+    // We can now use CORE.Application instead of QuizEngine
+    return _Page;
+});
