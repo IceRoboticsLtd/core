5099080c96ebf2c76b862f625c8ffe9bbba6ece9
wvanheemstra <wvanheemstra@icloud.com>
2015-03-07 13:43:12 +0000

    Updated

--- pre-public/js/app/pageController/pageController.js
+++ pre-public/js/app/pageController/pageController.js
@@ -1,6 +1,22 @@
 /*
  * PageController
  */
+define(['./Base'], function (Base) {
+    console.log('CORE: pageController called');
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+    });  
+    // ORIGINAL var _PageController = new Base(uuid);  
+    var attributes = attributes || {};
+    var options = options || {};       
+    var _PageController = new Base(uuid, attributes, options); // extended for attributes & options to adopt Backbone       
+    return _PageController;
+});
+
+/* TEMP COMMENTED OUT - QuizEngine specific 
 QuizEngine.Helpers = QuizEngine.Helpers || {};
 QuizEngine.Helpers.SubAppManager = (function() {
     console.log('CORE: QuizEngine.Helpers.SubAppManager called');
@@ -36,3 +52,4 @@ QuizEngine.Helpers.SubAppManager = (function() {
     return manager;
 
 })();
+*/
