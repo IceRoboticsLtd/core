fe5122442d84c28374d0b597cfdef4164e2b9756
wvanheemstra <wvanheemstra@icloud.com>
2015-03-11 17:29:40 +0000

    Updated

--- pre-public/js/booklibrary/models/book/Base.js
+++ pre-public/js/booklibrary/models/book/Base.js
@@ -6,6 +6,15 @@ define(function () {
     function BookBase(id, configs) {
         this.id = id;
         this.configs = configs;
+		var Book = CORE.main.model.Main.Model.extend({
+			defaults:{
+				coverImage:"http://localhost:4000/img/placeholder.png",
+				title:"No title",
+				author:"Unknown",
+				releaseDate:"Unknown",
+				keywords:"None"
+			}
+		});
     };
     BookBase.prototype = {
         getConfigs: function () {
