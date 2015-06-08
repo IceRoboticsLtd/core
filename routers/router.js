d99a72241dcc8502bb16379f7b4c0e78dda72d0e
Willem van Heemstra <wvanheemstra@icloud.com>
2015-06-08 17:08:37 +0100

    Updated

--- routers/router.js
+++ routers/router.js
@@ -1,8 +1,8 @@
 /*
  * Router
- */   
-module.exports = function(req, res, next) { 
-    var _Router = {};  
+ */
+module.exports = function(req, res, next) {
+    var _Router = {};
     config = require('../configs/server.js');
     var configs = config.configs,
         server_prefix = configs.server_prefix || 'CORE';
@@ -11,6 +11,7 @@ module.exports = function(req, res, next) {
     _Router.index = require('./index.js');
     _Router.login = require('./login.js');
     _Router.logout = require('./logout.js');
+    _Router.source = require('./source.js');
     _Router.test = require('./test.js');
     return _Router;
 }();// calls itself
